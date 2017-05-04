33e420d2ce5b5a764dd794690e28bf30bc9437a3
raman049 <raman049@yahoo.com>
2017-05-04 04:28:04 -0700

    Add files via upload

---
+++ Activity1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Content.PM;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Microsoft.Xna.Framework;
+
+namespace com.dragon_boat.Droid
+{
+	[Activity(Label = "com.dragon_boat.Droid",
+			   MainLauncher = true,
+			   Icon = "@drawable/icon",
+			   Theme = "@style/Theme.Splash",
+				AlwaysRetainTaskState = true,
+			   LaunchMode = LaunchMode.SingleInstance,
+			   ConfigurationChanges = ConfigChanges.Orientation |
+									  ConfigChanges.KeyboardHidden |
+									  ConfigChanges.Keyboard)]
+	public class Activity1 : AndroidGameActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			// Create our OpenGL view, and display it
+			//Game1.Activity = this;
+			//var g = new Game1();
+			//SetContentView(g.Window);
+			//g.Run();
+	var g = new Game1();
+
+	SetContentView((View)g.Services.GetService(typeof(View)));
+    g.Run();
+		}
+
+	}
+}
+
