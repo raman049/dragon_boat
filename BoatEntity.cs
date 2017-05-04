01dcbbaa1b3a0affd2786169a23bba022b418b3b
raman049 <raman049@yahoo.com>
2017-05-04 04:26:59 -0700

    Add files via upload

---
+++ BoatEntity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace com.dragon_boat
+{
+	public class BoatEntity
+	{
+
+		static Texture2D boatForward;
+		BoatRectangle boatRectangle;
+		public float X
+		{
+			get;
+			set;
+		}
+
+		public float Y
+		{
+			get;
+			set;
+		}
+		public GraphicsDevice GraphicsDevice { get; private set; }
+
+
+		public BoatEntity(GraphicsDevice graphicsDevice)
+		{
+			boatRectangle = new BoatRectangle();
+			if (boatForward == null)
+			{
+				using (var stream = TitleContainer.OpenStream("Content/boatForward.png"))
+				{
+					boatForward = Texture2D.FromStream(graphicsDevice, stream);
+
+
+
+				}
+			}
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			//	Vector2 topLeftOfSprite = new Vector2(boatRectangle.X, boatRectangle.Y);
+			Color tintColor = Color.White;
+			Rectangle boatRectangle1 = boatRectangle.Boat1Rectangle();
+			spriteBatch.Draw(boatForward, boatRectangle1, tintColor);
+
+		}
+
+
+	}
+}
---
+++ Game1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace com.dragon_boat
+{
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		BoatEntity boatF;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+			boatF = new BoatEntity(this.GraphicsDevice);
+			boatF.X = 100;
+			boatF.Y = 100;
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+			//using (var stream = TitleContainer.OpenStream("Content/boatForward.png"))
+			//{
+			//	boatForward = Texture2D.FromStream(this.GraphicsDevice, stream);
+			//}
+			//TODO: use this.Content to load your game content here 
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			// For Mobile devices, this logic will close the Game when the Back button is pressed
+			// Exit() is obsolete on iOS
+#if !__IOS__ && !__TVOS__
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+#endif
+
+			// TODO: Add your update logic here
+		//	boatF.X = boatF.X + 1;
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			//TODO: Add your drawing code here
+			spriteBatch.Begin();
+			//Vector2 topLeftOfSprite = new Vector2(50, 50);
+			//Color tintColor = Color.White;
+			//spriteBatch.Draw(boatForward, topLeftOfSprite, tintColor);
+			boatF.Draw(spriteBatch);
+
+			spriteBatch.End();
+			base.Draw(gameTime);
+		}
+
+		                    
+
+	}
+}
