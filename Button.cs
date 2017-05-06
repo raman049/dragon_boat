4ec478ae022d40db6e2d37bbe1a1bbf531521526
raman049 <raman049@yahoo.com>
2017-05-05 21:38:56 -0700

    Add files via upload

---
+++ Button.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input.Touch;
+namespace com.dragon_boat
+
+{
+	public class Button
+	{
+		
+		Rectangle buttonRectangle;
+		Texture2D SimpleTexture;
+
+	public Button(GraphicsDevice graphicsDevice, Rectangle bRect)
+		{
+			buttonRectangle = bRect;
+			SimpleTexture  = new Texture2D(graphicsDevice, 1, 1);
+			SimpleTexture.SetData(new[] { Color.White });
+		}
+public bool touchSelect(TouchCollection touchCollection)
+		{
+			Rectangle target = buttonRectangle;
+			//touchCollection = TouchPanel.GetState();
+			foreach (TouchLocation tl in touchCollection)
+			{
+				if (tl.State == TouchLocationState.Pressed && tl.Position.X < target.Right && tl.Position.X > target.Left &&
+					tl.Position.Y < target.Bottom && tl.Position.Y > target.Top)
+				{
+					return true;
+				}
+			} 
+			return false;
+
+		}
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			Color tintColor = Color.White;
+			spriteBatch.Draw(SimpleTexture, buttonRectangle, tintColor);
+		}
+	}
+}
