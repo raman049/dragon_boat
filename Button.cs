70559690fdd6f14aed8c4f89be58bb1262fa019a
raman049 <raman049@yahoo.com>
2017-05-07 03:16:25 -0700

    Update Button.cs

--- Button.cs
+++ Button.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input.Touch;
@@ -7,35 +7,46 @@ namespace com.dragon_boat
 {
 	public class Button
 	{
-		
 		Rectangle buttonRectangle;
 		Texture2D SimpleTexture;
+		Boolean output;
 
-	public Button(GraphicsDevice graphicsDevice, Rectangle bRect)
+		public Button(GraphicsDevice graphicsDevice, Rectangle bRect)
 		{
 			buttonRectangle = bRect;
-			SimpleTexture  = new Texture2D(graphicsDevice, 1, 1);
+			SimpleTexture = new Texture2D(graphicsDevice, 1, 1);
 			SimpleTexture.SetData(new[] { Color.White });
 		}
-public bool touchSelect(TouchCollection touchCollection)
+
+		public bool touchSelect(TouchCollection touchCollection)
 		{
 			Rectangle target = buttonRectangle;
-			//touchCollection = TouchPanel.GetState();
 			foreach (TouchLocation tl in touchCollection)
 			{
 				if (tl.State == TouchLocationState.Pressed && tl.Position.X < target.Right && tl.Position.X > target.Left &&
 					tl.Position.Y < target.Bottom && tl.Position.Y > target.Top)
 				{
-					return true;
+					output = true;
 				}
-			} 
-			return false;
-
+				else if (tl.State == TouchLocationState.Released)
+				{
+					output = false;
+				}
+			}
+			return output;
 		}
+
 		public void Draw(SpriteBatch spriteBatch)
 		{
 			Color tintColor = Color.White;
 			spriteBatch.Draw(SimpleTexture, buttonRectangle, tintColor);
 		}
+
+		public void DrawT2dButton(SpriteBatch spriteBatch, Texture2D t2d)
+		{
+			Color tintColor = Color.White;
+			spriteBatch.Draw(t2d, buttonRectangle, tintColor);
+		}
+
+		}
 	}
-}
