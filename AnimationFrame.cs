8a686775bcb3946fdf32b15f020b4c20b6d90f87
raman049 <raman049@yahoo.com>
2017-05-07 07:28:15 -0700

    Add files via upload

---
+++ AnimationFrame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace com.dragon_boat
+{
+	public class AnimationFrame
+	{
+		public Rectangle SourceRectangle { get; set; }
+		public TimeSpan Duration { get; set; }
+
+	}
+}
