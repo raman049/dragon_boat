8bad1672d1d8ff98cbb7e6fd631d178500232afa
raman049 <raman049@yahoo.com>
2017-05-07 03:15:40 -0700

    Update Game1.cs

--- Game1.cs
+++ Game1.cs
@@ -14,28 +14,32 @@ namespace com.dragon_boat
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 		Bounce boatF;
+		Home home;
 
 		public Game1()
 		{
 			graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
+
 		}
 
 		protected override void Initialize()
 		{
 			// TODO: Add your initialization logic here
 			boatF = new Bounce(this.GraphicsDevice);
-
-
+			home = new Home(this.GraphicsDevice);
 			base.Initialize();
 		}
 		protected override void LoadContent()
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch(GraphicsDevice);
+			home.LoadContent(Content);
+			boatF.LoadContent(Content);
+
 		}
 
-
+		Boolean Page2boolen;
 		protected override void Update(GameTime gameTime)
 		{
 			// For Mobile devices, this logic will close the Game when the Back button is pressed
@@ -47,8 +51,20 @@ namespace com.dragon_boat
 
 			// TODO: Add your update logic here
 			//	boatF.X = boatF.X + 1;
-			boatF.CheckCollision(this.GraphicsDevice);
-			boatF.updateButton(this.GraphicsDevice);  
+
+			if (home.Play_button)
+			{
+				Page2boolen = true;
+			}
+			if (home.Play_button == false)
+			{
+				home.updateButton(this.GraphicsDevice);
+			}
+			if (Page2boolen == true)
+			{
+				boatF.CheckCollision(this.GraphicsDevice);
+				boatF.updateButton(this.GraphicsDevice); 
+			}
 			base.Update(gameTime);
 		}
 		protected override void Draw(GameTime gameTime)
@@ -57,10 +73,16 @@ namespace com.dragon_boat
 
 			//TODO: Add your drawing code here
 			spriteBatch.Begin();
-			//Vector2 topLeftOfSprite = new Vector2(50, 50);
 			//Color tintColor = Color.White;
 			//spriteBatch.Draw(boatForward, topLeftOfSprite, tintColor);
-			boatF.Draw(spriteBatch); 
+			if (Page2boolen == true)
+			{
+				boatF.Draw(spriteBatch);
+			}if (Page2boolen == false)
+			{
+				home.Draw(spriteBatch);
+			}
+
 			spriteBatch.End();
 			base.Draw(gameTime);
 		}
