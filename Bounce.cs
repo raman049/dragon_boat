d5611920af4095ed132d1015cf1edf9681567714
raman049 <raman049@yahoo.com>
2017-05-08 00:37:49 -0700

    Update Bounce.cs

--- Bounce.cs
+++ Bounce.cs
@@ -8,12 +8,12 @@ namespace com.dragon_boat
 {
 	public class Bounce
 	{
-		Texture2D t2dBoat, whiteRectangle;
-		Rectangle ballBounds, boatRect;
-		Vector2 ballPosition, ballVelocity, boatPostion;
+		Texture2D t2dBoat, whiteRectangle, t2dFinishLine;
+		Rectangle bllBounds, boatRect, finishLineRect;
+		Vector2 ballPosition, boatPostion;
 		Button button1, button2;
 		Animation animationRight, animationLeft, animationForward, currentAnimation, animationSteady;
-
+		int width, height;
 		SpriteFont font;
 
 		public float X { get; set; }
@@ -22,6 +22,8 @@ namespace com.dragon_boat
 
 		public Bounce(GraphicsDevice graphicsDevice)
 		{
+			height = graphicsDevice.Viewport.Height;
+			width = graphicsDevice.Viewport.Width;
 			boatPostion = new Vector2(graphicsDevice.Viewport.Width / 2,
 									   graphicsDevice.Viewport.Height * 0.85f);
 			//ballVelocity = new Vector2(4, 10);
@@ -46,17 +48,24 @@ namespace com.dragon_boat
 			animationForward.AddFrame(new Rectangle(0, 0, 175, 510), TimeSpan.FromSeconds(.25));
 			animationForward.AddFrame(new Rectangle(175 * 3, 0, 175, 510), TimeSpan.FromSeconds(.25));
 
-			//ballBounds = new Rectangle((int)(ballPosition.X - ballTexture.Width / 2),
-			//(int)(ballPosition.Y - ballTexture.Height / 2), graphicsDevice.Viewport.Width / 10, graphicsDevice.Viewport.Height / 10);
-			boatRect = new Rectangle((int)(boatPostion.X - t2dBoat.Width / 2),
-			(int)(boatPostion.Y - t2dBoat.Height / 2), graphicsDevice.Viewport.Width / 10, graphicsDevice.Viewport.Height * 3 / 10);
-			whiteRectangle = new Texture2D(graphicsDevice, 1, 1);
-			whiteRectangle.SetData(new[] { Color.White });
+			if (t2dFinishLine == null)
+			{
+				using (var stream = TitleContainer.OpenStream("Content/checkers.png"))
+				{
+					t2dFinishLine = Texture2D.FromStream(graphicsDevice, stream);
+				}
+			}
+					boatRect = new Rectangle((int)(boatPostion.X - t2dBoat.Width / 2),
+				(int)(boatPostion.Y - t2dBoat.Height / 2), graphicsDevice.Viewport.Width / 10, graphicsDevice.Viewport.Height * 3 / 10);
+				finishLineRect = new Rectangle(0,20, graphicsDevice.Viewport.Width, (int)(graphicsDevice.Viewport.Height*0.02));
+		
+				whiteRectangle = new Texture2D(graphicsDevice, 1, 1);
+				whiteRectangle.SetData(new[] { Color.White });
 
-			button1 = new Button(graphicsDevice, new Rectangle(0, 0, graphicsDevice.Viewport.Width / 2, graphicsDevice.Viewport.Height));
-			button2 = new Button(graphicsDevice, new Rectangle(graphicsDevice.Viewport.Width / 2, 0, graphicsDevice.Viewport.Width / 2, graphicsDevice.Viewport.Height));
+				button1 = new Button(graphicsDevice, new Rectangle(0, 0, graphicsDevice.Viewport.Width / 2, graphicsDevice.Viewport.Height));
+				button2 = new Button(graphicsDevice, new Rectangle(graphicsDevice.Viewport.Width / 2, 0, graphicsDevice.Viewport.Width / 2, graphicsDevice.Viewport.Height));
 
-		}
+			}
 		public void LoadContent(ContentManager content)
 		{
 			font = content.Load<SpriteFont>("AlphaWoodFont");
@@ -64,22 +73,20 @@ namespace com.dragon_boat
 
 		public void CheckCollision(GraphicsDevice graphicsDevice)
 		{
-			if (ballBounds.Intersects(boatRect) || !graphicsDevice.Viewport.Bounds.Contains(ballBounds))
+			if (!graphicsDevice.Viewport.Bounds.Contains(boatRect))
 			{
-				ballVelocity = -ballVelocity;
-				ballPosition += ballVelocity;
+				boatPostion.X = graphicsDevice.Viewport.Width / 2;
+				boatPostion.Y = graphicsDevice.Viewport.Height * 0.7f;
+				//ballVelocity = -ballVelocity;
+				//ballPosition += ballVelocity;
 			}
 
 			else
 			{
 				//move the ball a bit
-				ballPosition += ballVelocity;
+				//ballPosition += ballVelocity;
 
 			}
-
-			//update bounding boxes
-			ballBounds.X = (int)ballPosition.X;
-			ballBounds.Y = (int)ballPosition.Y;
 
 
 		}
@@ -87,6 +94,8 @@ namespace com.dragon_boat
 		public void Draw(SpriteBatch spriteBatch)
 		{
 			Color tintColor = Color.White;
+			Color backgroundColor = new Color(113, 247, 236);
+			spriteBatch.GraphicsDevice.Clear(backgroundColor);
 			//spriteBatch.Draw(whiteRectangle, ballBounds, tintColor);
 			//spriteBatch.Draw(whiteRectangle, boatRect, tintColor);
 			//	spriteBatch.Draw(t2dBoat, boatRect, tintColor);
@@ -96,8 +105,19 @@ namespace com.dragon_boat
 			//button2.Draw(spriteBatch);
 			spriteBatch.DrawString(font, "text to 23423423423423423 print", new Vector2(40, 40), Color.Red);
 			var sourceRectangle = currentAnimation.CurrentRectangle;
-
 			spriteBatch.Draw(t2dBoat, boatRect, sourceRectangle, Color.White);
+			//spriteBatch.Draw(whiteRectangle, finishLineRect, Color.White);
+			//spriteBatch.Draw(t2dFinishLine, new Rectangle(0,0,10,10),new Rectangle(0,0,100,100) , Color.White);
+			drawFinishLine(spriteBatch);
+		}
+
+		public void drawFinishLine(SpriteBatch spriteBatch)
+		{ for (int i = 0; i < width/2; i++)
+			{
+				int size = (int)(height * 0.02);
+				spriteBatch.Draw(t2dFinishLine, new Rectangle(size*i,finishLineRect.Y,size,size),new Rectangle(0,0,100,100) , Color.White);
+			}
+		
 		}
 
 		public void moveRight()
@@ -146,27 +166,27 @@ namespace com.dragon_boat
 				boolean_br = true;
 				if (button1.touchSelect(touchCollection))
 				{
-					boolean_bl= true;
+					boolean_bl = true;
 				}
 			}
-				
+
 			if (boolean_bl == true && boolean_br == true)
+			{
+				moveForward();
+				currentAnimation = animationForward;
+				Console.WriteLine("button 1 && button 2");
+				boolean_br = false;
+				boolean_bl = false;
+			}
+			else
+			{
+				if (boolean_bl == true)
 				{
-					moveForward();
-					currentAnimation = animationForward;
-					Console.WriteLine("button 1 && button 2");
-					boolean_br = false;
+					moveLeft();
+					currentAnimation = animationLeft;
+					Console.WriteLine("button 1");
 					boolean_bl = false;
 				}
-				else
-				{
-					if (boolean_bl == true)
-					{
-						moveLeft();
-						currentAnimation = animationLeft;
-						Console.WriteLine("button 1");
-						boolean_bl = false;
-					}
 				if (boolean_br == true)
 				{
 					moveRight();
@@ -175,15 +195,16 @@ namespace com.dragon_boat
 					boolean_br = false;
 				}
 				else
-				{currentAnimation = animationSteady;
+				{
+					currentAnimation = animationSteady;
 				}
-				}
-
-				boatRect.X = (int)boatPostion.X;
-				boatRect.Y = (int)boatPostion.Y;
-				currentAnimation.Update(gameTime);
 			}
 
+			boatRect.X = (int)boatPostion.X;
+			boatRect.Y = (int)boatPostion.Y;
+			currentAnimation.Update(gameTime);
 		}
+
+	}
 
 }
